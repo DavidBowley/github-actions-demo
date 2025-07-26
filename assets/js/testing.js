2532242b934833417b47bb496141b9a53f93a733
DavidBowley <DavidBowley@users.noreply.github.com>
2025-07-26 23:24:18 +0000

    Deploying to gh-pages from @ DavidBowley/github-actions-demo@49925f0a1c66eefdf5dc963a338cd5d221b59a21 🚀

--- assets/js/testing.js
+++ assets/js/testing.js
@@ -2,6 +2,6 @@
 function myTestFunction(arg1) {
     console.log(arg1);
 }
-const myTest = "test function ran - time: 21:37";
+const myTest = "test function ran... Time: 00:14";
 myTestFunction(myTest);
 //# sourceMappingURL=testing.js.map
--- assets/js/testing.ts
+++
@@ -1,7 +0,0 @@
-function myTestFunction(arg1: string) {
-  console.log(arg1);
-}
-
-const myTest = "test function ran - time: 21:37";
-
-myTestFunction(myTest);
