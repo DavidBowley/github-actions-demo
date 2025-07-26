8f656e756aa51d2c7d9985843af570c5007880e0
David Bowley <46280190+DavidBowley@users.noreply.github.com>
2025-07-26 21:43:44 +0100

    Testing 11ty build in GH Actions

--- src/assets/js/testing.ts
+++ src/assets/js/testing.ts
@@ -2,6 +2,6 @@ function myTestFunction(arg1: string) {
   console.log(arg1);
 }
 
-const myTest = "test function ran - time: 21:08";
+const myTest = "test function ran - time: 21:37";
 
 myTestFunction(myTest);
