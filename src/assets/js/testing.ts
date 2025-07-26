73677133eb209e0243790f04fde79cf239c4bcae
David Bowley <46280190+DavidBowley@users.noreply.github.com>
2025-07-26 19:37:57 +0100

    Testing that pushes don't deploy

--- src/assets/js/testing.ts
+++ src/assets/js/testing.ts
@@ -2,6 +2,6 @@ function myTestFunction(arg1: string) {
   console.log(arg1);
 }
 
-const myTest = "test function ran";
+const myTest = "test function ran - made a change";
 
 myTestFunction(myTest);
