da68214e2c30b97781e5cfc88964a032f6b91194
David Bowley <46280190+DavidBowley@users.noreply.github.com>
2025-07-26 21:09:36 +0100

    testing deployment

--- src/assets/js/testing.ts
+++ src/assets/js/testing.ts
@@ -2,6 +2,6 @@ function myTestFunction(arg1: string) {
   console.log(arg1);
 }
 
-const myTest = "test function ran - changed back";
+const myTest = "test function ran - time: 21:08";
 
 myTestFunction(myTest);
