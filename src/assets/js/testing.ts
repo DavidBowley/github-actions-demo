8d6216f1936973bf0e95b315d4bf4dd19ecd4876
David Bowley <46280190+DavidBowley@users.noreply.github.com>
2025-07-26 19:47:23 +0100

    Testing branch deploy

--- src/assets/js/testing.ts
+++ src/assets/js/testing.ts
@@ -2,6 +2,6 @@ function myTestFunction(arg1: string) {
   console.log(arg1);
 }
 
-const myTest = "test function ran - made a change";
+const myTest = "test function ran - changed back";
 
 myTestFunction(myTest);
