51688c23ddfa2126354014ed3f3a2238c2621964
DavidBowley <DavidBowley@users.noreply.github.com>
2025-07-26 19:22:25 +0000

    Deploying to gh-pages from @ DavidBowley/github-actions-demo@3de89ba6654232b23ffb2c3ab74b96f35cd2bb0e 🚀

---
+++ assets/js/testing.js
@@ -0,0 +1,7 @@
+"use strict";
+function myTestFunction(arg1) {
+    console.log(arg1);
+}
+const myTest = "test function ran - changed back";
+myTestFunction(myTest);
+//# sourceMappingURL=testing.js.map
--- eleventy.config.mjs
+++
@@ -1,5 +0,0 @@
-export default async function (eleventyConfig) {
-  eleventyConfig.setInputDirectory("src");
-  eleventyConfig.setOutputDirectory("build");
-  eleventyConfig.addPassthroughCopy("src/assets");
-}
