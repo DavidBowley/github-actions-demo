ba71249fd99690632f8edb726abd866069cfab63
David Bowley <46280190+DavidBowley@users.noreply.github.com>
2025-07-27 01:34:37 +0100

    Removing working directory for tsc as seems not needed / tidying up .yml file

--- src/assets/js/testing.ts
+++ src/assets/js/testing.ts
@@ -2,6 +2,6 @@ function myTestFunction(arg1: string) {
   console.log(arg1);
 }
 
-const myTest = "test function ran... Time: 00:14";
+const myTest = "test function ran... Time: 01:27";
 
 myTestFunction(myTest);
