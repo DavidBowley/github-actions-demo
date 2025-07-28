78f2b4bb321de1de517e60a08327f8086a4f94bc
David Bowley <46280190+DavidBowley@users.noreply.github.com>
2025-07-28 22:10:51 +0100

    Testing 11ty folder structure

--- src/assets/js/testing.ts
+++ src/assets/js/testing.ts
@@ -2,6 +2,6 @@ function myTestFunction(arg1: string) {
   console.log(arg1);
 }
 
-const myTest = "test function ran... Time: 01:27";
+const myTest = "test function ran... Time: 21:44";
 
 myTestFunction(myTest);
